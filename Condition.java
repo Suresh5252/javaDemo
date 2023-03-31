66d3440b7c7fdebfe97903ed5104c050daf280f9
Suresh <sureshvasanth33@gmail.com>
2023-03-31 07:27:35 +0530

    file added

---
+++ Condition.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Condition{
+
+public static void main(String args[]){
+
+
+Scanner obj = new Scanner(System.in);
+
+int male = obj.nextInt();
+int female = obj.nextInt();
+
+if(male > 23){
+
+if(female > 21){
+
+System.out.println("Eligible to have marriage");
+
+}else{
+System.out.println("Not Eligible to have marriage");
+}
+}
+else{
+System.out.println("Not Eligible to have marriage");
+}
+}
+}
+
+
